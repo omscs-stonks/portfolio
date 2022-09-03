a013b828ab822a57b686ca264ba7e53089b7253b
Stonks <112432332+omscs-stonks@users.noreply.github.com>
2022-09-03 16:26:33 -0700

    Update stocks-plot.py

--- stocks-plot.py
+++ stocks-plot.py
@@ -5,8 +5,6 @@ import plotly.express as px
 import matplotlib.pyplot as plt
 pd.options.plotting.backend = "plotly"
 
-
-
 FPATH = 'portfolio\portfolio.json'
 
 def _load_portfolio_data(fpath):
@@ -38,30 +36,6 @@ def _loading_ticker_data(stocks):
     return df_adj
 
 
-def plot_daily_rets(**kwargs):
-    """
-    Loading the combined ticker data from yahoo finance unoffical API
-    Using the adjusted close to get the daily close value for each ticker
-    Dropping rows for Holidays/Weekends
-
-    Input: string of space seperated stock tickers
-    Output: Pandas Dataframe of adjusted closed values
-    """
-    df = kwargs["df"]
-    fig = df.plot(title=kwargs["title"], template="simple_white",
-              labels=dict(index=kwargs["idx"], value=kwargs["val"], variable=kwargs["var"]))
-    fig.update_yaxes(tickprefix="%")
-    fig.show()
-
-    fig = px.line(df, x=kwargs["idx"], y=df.columns,
-              hover_data={"date": "|%B %d, %Y"},
-              title=kwargs["title"])
-    fig.update_xaxes(
-        dtick="M1",
-        tickformat="%b\n%Y")
-    fig.show()
-
-
 def cumulative_port_rets(prices, weights):
     """
     Input: string of space seperated stock tickers
@@ -74,6 +48,10 @@ def cumulative_port_rets(prices, weights):
     port_ret  = pd.DataFrame(daily_ret.values * weights.values, columns=daily_ret.columns, index=daily_ret.index)
     port_ret = port_ret.sum(axis = 1)
     cum_port_ret = (port_ret + 1).cumprod()
+    cum_port_ret = cum_port_ret.to_frame()
+    cum_port_ret.reset_index(level=0, inplace=True)
+    cum_port_ret.columns = ['Date', 'Return']
+
     return cum_port_ret
 
 def cumulative_tkr_rets(prices):
@@ -84,7 +62,7 @@ def cumulative_tkr_rets(prices):
     Output: Pandas Dataframe of daily returns by ticker
     """
     daily_ret = prices.drop(['AFRM'], axis=1).pct_change().fillna(0)
-    cum_daily_ret = (daily_ret + 1).cumprod()
+    cum_daily_ret = (daily_ret + 1.0 ).cumprod()
     return cum_daily_ret
 
 def extact_data(fpath):
@@ -98,34 +76,24 @@ def extact_data(fpath):
     prices = _loading_ticker_data(stocks=stocks)
     shares = pd.DataFrame(pf).drop('Start_Date').drop(['AFRM'], axis=1)
     weights = shares.div(shares.sum(axis=1), axis=0)
+    weights = weights.astype(float)
 
     return prices, weights
 
-prices, weights = extact_data(FPATH)
-cum_port_ret = cumulative_port_rets(prices, weights)
-cum_port_ret.columns
+def plot_tickers(tkr_df):
+    fig = tkr_df.plot(title="Pandas Backend Example", template="simple_white",
+                labels=dict(index="time", value="Return", variable="Ticker"))
+    fig.update_yaxes(tickprefix="%")
+    fig.write_html("ticker-daily.html")
 
-
-cum_tkr_ret = cumulative_tkr_rets(prices)
-cum_tkr_ret.reset_index(level=0, inplace=True)
-
-fig = px.line(cum_tkr_ret, x="Date", y=cum_tkr_ret.columns ,
-              hover_data={"Date": "|%B %d, %Y"},
-              title='Daily Cumulative Ticker Level Return (YTD)' )
-
-fig.update_xaxes(
-    dtick="M1",
-    tickformat="%b\n%Y")
-fig.show()
-
-fig.write_html("path/to/file.html")
-
+def plot_portfolio(port_df):
+    fig = px.line(port_df, x="Date", y="Return")
+    fig.write_html("portfolio-daily.html")
 
 if __name__ == '__main__':
 
-
-    # plot_daily_rets(**{"df": daily_ret, "title": "Cumulative TICKER Daily Return",
-    #                     "idx": "Date",  "val":"""% Return""", "var":"TICKER" })
-
-    plot_daily_rets(**{"df": cum_port_ret, "title": "Cumulative Portfolio Daly Return",
-                        "idx": "Date",  "val":"""% Return""", "var":"Portfolio" })
+    prices, weights = extact_data(FPATH)
+    cum_port_ret = cumulative_port_rets(prices, weights)
+    cum_tkr_ret = cumulative_tkr_rets(prices)
+    plot_tickers(cum_tkr_ret)
+    plot_portfolio(cum_port_ret)
